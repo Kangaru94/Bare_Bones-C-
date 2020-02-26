b0694d6294cd7c2e47af536b3bfa459e28506bd4
Kangaru <46432989+Kangaru94@users.noreply.github.com>
2020-02-25 18:55:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Stephen";
+            Console.WriteLine($"Hello, {name}!");
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine($"Your name has {name.Length} characters");        
+        }
+    }
+}
