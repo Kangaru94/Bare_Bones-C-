39edf2f89cb0dde19b7a639f2af35c76510660c3
Kangaru <46432989+Kangaru94@users.noreply.github.com>
2020-02-26 19:00:35 -0600

    Update Program.cs
    Added comments. They're // instead of # or ''' !
    
    Did a Do loop to compare and contrast Do and While loops. Useful difference.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace HelloWorld
 {
@@ -6,10 +6,52 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //two variables, an int for the counter and a string for the if/else
+            int counter = 0;
             string name = "Stephen";
+
+            //basic WriteLine
             Console.WriteLine($"Hello, {name}!");
             Console.WriteLine(name.ToUpper());
-            Console.WriteLine($"Your name has {name.Length} characters");        
+            Console.WriteLine($"Your name has {name.Length} characters");
+
+            //my if/else statement
+            //outputs "YES" and "Is Stephen" if variable "name" == "Stephen"
+            if (name == "Stephen")
+            {
+                Console.WriteLine("YES");
+                Console.WriteLine("Is Stephen");
+            }
+
+            //else it outputs "No" and "Is not Stephen"
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Is not Stephen");
+            }
+
+            //first instance of a Do loop
+            //shows it runs before the condition is met, as the counter isn't  increased yet
+            do
+            {
+                Console.WriteLine("Do Loop Scoop Woop");
+            } while (counter == 10);
+
+            //the while loop simply counts to 10
+            //doesn't run unless the condition is met
+            while (counter <= 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            //Shows the Do loop will run again if the condition is met
+            do
+            {
+                Console.WriteLine("Do Loop Scoop Woop");
+            } while (counter == 10);
+
         }
+
     }
 }
